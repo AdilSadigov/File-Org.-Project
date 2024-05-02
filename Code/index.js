b09eb1138eab0be71ee5fbc164182c56cfcacdc0
Adil <sadiqovadil9@gmail.com>
2024-05-02 09:46:01 +0300

    fixed bugs

--- Code/index.js
+++ Code/index.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
+am = 1
 
 // Dosya okuma ve parolaları indexleme fonksiyonu
 function indexPasswords(directory) {
   const files = fs.readdirSync(directory);
-  const indexedPasswords = new Set(); // Bir kez indekslenen şifreleri takip etmek için bir Set kullanıyoruz
   
   files.forEach(file => {
     const filePath = path.join(directory, file);
@@ -15,7 +15,7 @@ function indexPasswords(directory) {
     lines.forEach(line => {
       const password = line.trim();
 
-      if (password && !indexedPasswords.has(password)) { // Eğer şifre daha önce indekslenmediyse devam et
+      if (password) { // Eğer şifre daha önce indekslenmediyse devam et
         let firstChar = password.charAt(0).toLowerCase();           
         
         if (/[^^\w\d]/.test(firstChar)) {
@@ -28,11 +28,11 @@ function indexPasswords(directory) {
         let indexFilePath = path.join(indexFolder, fileName);
         
         fs.mkdirSync(indexFolder, { recursive: true });
-        
+          
         while (fs.existsSync(indexFilePath)) {
-          const data = fs.readFileSync(indexFilePath, 'utf8');
-          const lines = data.split('\n');
-          const fileSizeInLines = lines.length;
+          const dataCurrent = fs.readFileSync(indexFilePath, 'utf8');
+          const linesCurrent = dataCurrent.split('\n');
+          const fileSizeInLines = linesCurrent.length;
           
           if (fileSizeInLines > 10000) {
             index++;
@@ -45,23 +45,20 @@ function indexPasswords(directory) {
         for (let i = 1; i <= index; i++) {
           indexPath4Control = path.join(indexFolder, `${firstChar}-passwords${i}.txt`)
 
-          if (typeof isPasswordInFile === 'undefined') {
+          const isPasswordExists = isPasswordInFile(password, indexPath4Control);           
+
+          if (isPasswordExists) {
+            break
+          } else {  // Şifrenin dosyada olup olmadığını kontrol et
             const passwordEntry = `${password}|${hash(password, 'md5')}|${hash(password, 'sha1')}|${hash(password, 'sha256')}|${file}`;
-            fs.appendFileSync(indexFilePath, passwordEntry + '\n');
-            indexedPasswords.add(password); // Şifreyi indekslendi olarak işaretle
-          } else {
-            const isPasswordExists = isPasswordInFile(password, indexFilePath);           
-            // Şifrenin dosyada olup olmadığını kontrol et
-            if (!isPasswordExists) {
-              const passwordEntry = `${password}|${hash(password, 'md5')}|${hash(password, 'sha1')}|${hash(password, 'sha256')}|${file}`;
-              fs.appendFileSync(indexFilePath, passwordEntry + '\n');
-            }
-          }
+            fs.appendFileSync(indexFilePath, passwordEntry + '\n');         
+          }            
         }      
       }
     });
   }); 
 }
+
 
 // Hash fonksiyonu
 function hash(text, algorithm) {
@@ -69,17 +66,17 @@ function hash(text, algorithm) {
 }
 
 // Dosya içinde şifrenin var olup olmadığını kontrol etme fonksiyonu
-// function isPasswordInFile(password, filePath) {
-//   if (fs.existsSync(filePath)) {
-//     const data = fs.readFileSync(filePath, 'utf8');
-//     const lines = data.split('\n');
-//     return lines.some(line => {
-//       const storedPassword = line.split('|')[0];
-//       return storedPassword === password;
-//     });
-//   }
-//   return false;
-// }
+function isPasswordInFile(password, filePath) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const lines = data.split('\n');
+    return lines.some(line => {
+      const storedPassword = line.split('|')[0];
+      return storedPassword === password;
+    });
+  }
+  return false;
+}
 
 // Dosya okuma ve parolaları indexleme işlemini başlatma
 indexPasswords(path.join(__dirname, '../Unprocessed-Passwords'));
